cc7bab5cbd3dddfba6f5bc16603b8efc250cb882
Leonid Ionov <neonych@gmail.com>
2020-08-02 23:26:45 +0300

    Updates

---
+++ precache-manifest.136931b919c94af173e790b2706b19a4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2aa63f955c6ce906035731e9f32c152",
+    "url": "/socialNetworkDemo/index.html"
+  },
+  {
+    "revision": "79c005cb3f50df2625b7",
+    "url": "/socialNetworkDemo/static/css/3.2897a420.chunk.css"
+  },
+  {
+    "revision": "266988cf9f53527b2fa5",
+    "url": "/socialNetworkDemo/static/css/4.0ab0ac3b.chunk.css"
+  },
+  {
+    "revision": "dfd4b16cd44d835258b6",
+    "url": "/socialNetworkDemo/static/css/5.b1e7e113.chunk.css"
+  },
+  {
+    "revision": "0762cb243e42bf4b8486",
+    "url": "/socialNetworkDemo/static/css/main.dcb4f6ac.chunk.css"
+  },
+  {
+    "revision": "2854eb50557315a7814d",
+    "url": "/socialNetworkDemo/static/js/2.8a7cb7e8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/socialNetworkDemo/static/js/2.8a7cb7e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79c005cb3f50df2625b7",
+    "url": "/socialNetworkDemo/static/js/3.8135abc7.chunk.js"
+  },
+  {
+    "revision": "266988cf9f53527b2fa5",
+    "url": "/socialNetworkDemo/static/js/4.ed498d76.chunk.js"
+  },
+  {
+    "revision": "dfd4b16cd44d835258b6",
+    "url": "/socialNetworkDemo/static/js/5.67745c23.chunk.js"
+  },
+  {
+    "revision": "0eb08a35ed68dc0ffb42",
+    "url": "/socialNetworkDemo/static/js/6.9fbdafd2.chunk.js"
+  },
+  {
+    "revision": "0762cb243e42bf4b8486",
+    "url": "/socialNetworkDemo/static/js/main.23c6cdd9.chunk.js"
+  },
+  {
+    "revision": "f5dc15368c0bfaf6589e",
+    "url": "/socialNetworkDemo/static/js/runtime-main.80e0157d.js"
+  },
+  {
+    "revision": "a8e4cd681be06f179870f54a19dbb0ff",
+    "url": "/socialNetworkDemo/static/media/Loading.a8e4cd68.svg"
+  },
+  {
+    "revision": "927d5861c3b68c52dd70336a981bad82",
+    "url": "/socialNetworkDemo/static/media/avatarSmall.927d5861.jpg"
+  },
+  {
+    "revision": "9e71b43048de541277cc3d8d66e24ce6",
+    "url": "/socialNetworkDemo/static/media/demon-face.9e71b430.jpg"
+  }
+]);
